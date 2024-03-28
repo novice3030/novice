cd2e4a0f086cfa0265cec5b11848334d4010e12a
ran-konnecto <ran@konnecto.io>
2024-03-28 12:22:35 +0200

    Added ui lib

--- apps/every-stack/src/app/app.component.ts
+++ apps/every-stack/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { CardComponent } from '@novice/ui';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, CardComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
